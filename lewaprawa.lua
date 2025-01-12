7a638f0e1cf5fd321ad63f5764c1a134c9d96bba
Prometeusz152 <149016687+Prometeusz152@users.noreply.github.com>
2025-01-12 13:36:54 +0100

    Update lewaprawa.lua

--- lewaprawa.lua
+++ lewaprawa.lua
@@ -83,9 +83,9 @@ addUICorner(Frame, 10)
 InfoLabel.Parent = Frame
 InfoLabel.BackgroundColor3 = Color3.fromRGB(26, 26, 26)
 InfoLabel.BorderSizePixel = 0 -- Remove the border
-InfoLabel.Position = UDim2.new(0, 0, 0, -50) -- Adjust position to be above the frame
-InfoLabel.Size = UDim2.new(1, 0, 0, 50) -- Adjust size to be slightly larger in height
-InfoLabel.Text = "ethereal. 0.2\n" .. plr.Name -- Display "ethereal. 0.1" and player name
+InfoLabel.Position = UDim2.new(-0.1, 0, 0, -50) -- Adjust position to be aligned with the frame
+InfoLabel.Size = UDim2.new(1.2, 0, 0, 50) -- Adjust size to be slightly larger in height
+InfoLabel.Text = "ethereal. 0.1\n" .. plr.Name -- Display "ethereal. 0.1" and player name
 InfoLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 InfoLabel.TextSize = 14 -- Set the text size to a smaller value
 InfoLabel.TextWrapped = true -- Wrap text to fit within the label
